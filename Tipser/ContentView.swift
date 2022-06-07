279b7c8d19938809d9b5913fe81f8b3bd0ceba58
vaGaidenko <va.gaidenko@gmail.com>
2022-06-07 16:09:47 +0700

    Initial Commit

---
+++ Tipser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tipser
+//
+//  Created by Владислав Гайденко on 07.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tipser/TipserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipserApp.swift
+//  Tipser
+//
+//  Created by Владислав Гайденко on 07.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TipserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
